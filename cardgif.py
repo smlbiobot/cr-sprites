df2149c6f5418775a2068995e2fe1e25db836bfb
smlbiobot <smlbiobot@gmail.com>
2019-12-09 17:03:13 +0800

    Add config, handle PIL exceptions

--- cardgif.py
+++ cardgif.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
+import json
 import os
+
+import PIL
 from PIL import Image
 
 # Animated GIF
 OUT_FOLDER = os.path.join(".", "out-gif")
-SC_FOLDER = os.path.join(
-    "..", "apk", "1.8.2", "com.supercell.clashroyale-1.8.2-decoded",
-    "assets", "sc")
+with open("./config.json") as f:
+    config = json.load(f)
+SC_FOLDER = os.path.join(*config['sc_folder']['paths'])
 
 for root, dirs, files in os.walk(SC_FOLDER):
     for dir_name in dirs:
@@ -17,8 +20,19 @@ for root, dirs, files in os.walk(SC_FOLDER):
             if os.path.isfile(os.path.join(folder, f))]
         file_paths = [
             os.path.join(folder, f) for f in files]
-        images = [
-            Image.open(f) for f in file_paths]
+
+        # images = [
+        #     Image.open(f) for f in file_paths]
+
+        images = []
+        for f in file_paths:
+            try:
+                image = Image.open(f)
+            except PIL.Image.DecompressionBombError:
+                continue
+            else:
+                images.append(image)
+
         out_file = os.path.join(OUT_FOLDER, "{}.gif".format(dir_name))
         if images:
             image = images[0]
--- cardstack.py
+++ cardstack.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python3
 
+import json
 import os
+
+import PIL
 from PIL import Image
 
 # Stacked image composite
 OUT_FOLDER = os.path.join(".", "out-stack")
-SC_FOLDER = os.path.join(
-    "..", "apk", "1.8.2", "com.supercell.clashroyale-1.8.2-decoded",
-    "assets", "sc")
+with open("./config.json") as f:
+    config = json.load(f)
+SC_FOLDER = os.path.join(*config['sc_folder']['paths'])
 
 for root, dirs, files in os.walk(SC_FOLDER):
     for dir_name in dirs:
+        if dir_name.startswith('_'):
+            continue
         folder = os.path.join(SC_FOLDER, dir_name)
         files = [
             f for f in os.listdir(folder)
             if os.path.isfile(os.path.join(folder, f))]
         file_paths = [
             os.path.join(folder, f) for f in files]
-        images = [
-            Image.open(f) for f in file_paths]
 
         image = None
-        for i, img in enumerate(images):
-            if i == 0:
-                image = img
-            else:
-                if img:
-                    image = Image.alpha_composite(image, img)
+        for i, file_path in enumerate(file_paths):
+            try:
+                img = Image.open(file_path)
+                if i == 0:
+                    image = img
+                else:
+                    if img and image:
+                        image = Image.alpha_composite(img, image)
+            except PIL.Image.DecompressionBombError:
+                continue
 
         out_file = os.path.join(OUT_FOLDER, "{}.png".format(dir_name))
         if image:
